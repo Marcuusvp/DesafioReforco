25800a63349271974ef6b44837f530cd9edc15e9
Marcuusvp <marcusviniciuspereira1@gmail.com>
2024-10-31 21:13:19 -0300

    removendo compose e ajustando exception caso um erro ocorra

--- src/Servicos/PropostasApi/Dominio/Proposta/Aplicacao/CriarProposta/CriarPropostaEndpoint.cs
+++ src/Servicos/PropostasApi/Dominio/Proposta/Aplicacao/CriarProposta/CriarPropostaEndpoint.cs
@@ -1,4 +1,6 @@
-﻿namespace PropostasApi.Dominio.Proposta.Aplicacao.CriarProposta
+﻿using PropostasApi.Exceptions;
+
+namespace PropostasApi.Dominio.Proposta.Aplicacao.CriarProposta
 {
     public record CriarPropostaRequest(string Cpf, decimal Rendimento, PropostaEnderecoDto Endereco, string Telefone, string Email, string Agente, string Conveniada, int TipoOperacao);
     public record PropostaEnderecoDto(string Logradouro, string Bairro, string Cep, int Numero, string Cidade, string Estado, string? Complemento);
@@ -12,6 +14,11 @@
                 var command = request.Adapt<CriarPropostaCommand>();
 
                 var result = await sender.Send(command);
+
+                if (result.IsFailure)
+                {
+                    throw new PropostaInvalidaException(result.Error);
+                }
 
                 var response = result.Adapt<CriarPropostaResponse>();
 
